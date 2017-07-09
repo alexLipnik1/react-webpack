07ae3c944e74174e76ad0ea45c6d8524a69ca38b
alex lipnik <alex.lipnik111@gmail.com>
2017-07-09 17:36:04 +0300

    added state control and update function and basic input functionality

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,7 +6,52 @@ import React from 'react';
 import * as RB from 'react-bootstrap';
 import styles from './App.scss';
 
+import {findDOMNode} from 'react-dom';
+
+
+class Input extends React.Component {
+  render(){
+    return <div><input ref="input" type="text" onChange={this.props.update}/></div>
+  }
+}
+
+
 export default class App extends React.Component {
+  state = {
+    client: '',
+    id: '',
+    address: '',
+    zip: '',
+    phone: '',
+    contect: '',
+    workDays: '',
+    hours: '',
+    workType: '',
+    payment: '',
+    total: '',
+    VAT: '',
+    totatWithVAT: '',
+  }
+
+  update = () => {
+    this.setState({
+
+      client: findDOMNode(this.refs.client).value,
+      id: findDOMNode(this.refs.id).value,
+      address: findDOMNode(this.refs.address).value,
+      zip: findDOMNode(this.refs.zip).value,
+      phone: findDOMNode(this.refs.phone).value,
+      contect: findDOMNode(this.refs.contect).value,
+      workDays: findDOMNode(this.fets.workDays).value,
+      hours: findDOMNode(this.refs.hours).value,
+      workType: findDOMNode(this.refs.workType).value,
+      payment: findDOMNode(this.refs.payment).value,
+      total: '',
+      VAT: '',
+      totatWithVAT: '',
+    })
+  }
+
   render() {
     return (
     <div className={styles.container}>
@@ -16,28 +61,28 @@ export default class App extends React.Component {
 
       <div className={styles.clientDetails}>
         <RB.FormGroup controlId="client">
-        <RB.ControlLabel>Client</RB.ControlLabel>{' '}
-        <RB.FormControl type="text" placeholder="Enter client name" />
+          <RB.ControlLabel>Client</RB.ControlLabel>{' '}
+          <RB.FormControl ref="client" onChange={this.update} type="text" placeholder="Enter client name" />
         </RB.FormGroup>
         <RB.FormGroup controlId="id">
-        <RB.ControlLabel>ID</RB.ControlLabel>{' '}
-        <RB.FormControl type="text" placeholder="Enter ID" />
+          <RB.ControlLabel>ID</RB.ControlLabel>{' '}
+          <RB.FormControl ref="id" onChange={this.update} type="text" placeholder="Enter ID" />
         </RB.FormGroup>
-        <RB.FormGroup controlId="address">
-        <RB.ControlLabel>Address</RB.ControlLabel>{' '}
-        <RB.FormControl type="text" placeholder="Enter Address" />
+          <RB.FormGroup controlId="address">
+          <RB.ControlLabel>Address</RB.ControlLabel>{' '}
+          <RB.FormControl ref="address" onChange={this.update} type="text" placeholder="Enter Address" />
         </RB.FormGroup>
         <RB.FormGroup controlId="zip">
-        <RB.ControlLabel>ZIP</RB.ControlLabel>{' '}
-        <RB.FormControl type="text" placeholder="Enter ZIP" />
+          <RB.ControlLabel>ZIP</RB.ControlLabel>{' '}
+          <RB.FormControl ref="client" onChange={this.update} type="text" placeholder="Enter ZIP" />
         </RB.FormGroup>
         <RB.FormGroup controlId="phone">
-        <RB.ControlLabel>Phone</RB.ControlLabel>{' '}
-        <RB.FormControl type="text" placeholder="Enter phone" />
+          <RB.ControlLabel>Phone</RB.ControlLabel>{' '}
+          <RB.FormControl ref="client" onChange={this.update} type="text" placeholder="Enter phone" />
         </RB.FormGroup>
         <RB.FormGroup controlId="contect">
-        <RB.ControlLabel>Contect</RB.ControlLabel>{' '}
-        <RB.FormControl type="text" placeholder="Enter Contect" />
+          <RB.ControlLabel>Contect</RB.ControlLabel>{' '}
+          <RB.FormControl ref="client" onChange={this.update} type="text" placeholder="Enter Contect" />
         </RB.FormGroup>
       </div>
 
@@ -47,36 +92,36 @@ export default class App extends React.Component {
         <div className={styles.payment}>
           <div className={styles.line}>
             <RB.FormGroup controlId="workDays">
-            <RB.ControlLabel>Work Days</RB.ControlLabel>{' '}
-            <RB.FormControl type="text" placeholder="" />
+              <RB.ControlLabel>Work Days</RB.ControlLabel>{' '}
+              <RB.FormControl ref="workDays" onChange={this.update}  type="text" placeholder="" />
             </RB.FormGroup>
             <RB.FormGroup controlId="hours">
-            <RB.ControlLabel>Hours</RB.ControlLabel>{' '}
-            <RB.FormControl type="text" placeholder="" />
+              <RB.ControlLabel>Hours</RB.ControlLabel>{' '}
+              <RB.FormControl ref="hours" onChange={this.update}  type="text" placeholder="" />
             </RB.FormGroup>
-            <RB.FormGroup controlId="workType">
-            <RB.ControlLabel>Work type</RB.ControlLabel>{' '}
-            <RB.FormControl type="text" placeholder="" />
+              <RB.FormGroup controlId="workType">
+              <RB.ControlLabel>Work type</RB.ControlLabel>{' '}
+              <RB.FormControl ref="workType" onChange={this.update}  type="text" placeholder="" />
             </RB.FormGroup>
           </div>
         </div>
 
         <div className={styles.TotalPayment}>
           <RB.FormGroup className={styles.paymentInput} controlId="paymant">
-          <RB.ControlLabel>Paymant</RB.ControlLabel>{' '}
-          <RB.FormControl type="text" placeholder="" />
+            <RB.ControlLabel>Paymant</RB.ControlLabel>{' '}
+            <RB.FormControl ref="payment" onChange={this.update}  type="text" placeholder="" />
           </RB.FormGroup>
           <RB.FormGroup controlId="finalTotal">
-          <RB.ControlLabel>Total</RB.ControlLabel>{' '}
-          <RB.FormControl type="text" placeholder="" />
+            <RB.ControlLabel>Total</RB.ControlLabel>{' '}
+            <RB.FormControl type="text" placeholder="" />
           </RB.FormGroup>
           <RB.FormGroup controlId="VAT">
-          <RB.ControlLabel>VAT</RB.ControlLabel>{' '}
-          <RB.FormControl type="text" placeholder="" />
+            <RB.ControlLabel>VAT</RB.ControlLabel>{' '}
+            <RB.FormControl type="text" placeholder="" />
           </RB.FormGroup>
           <RB.FormGroup controlId="total+VAT">
-          <RB.ControlLabel>Total+VAT</RB.ControlLabel>{' '}
-          <RB.FormControl type="text" placeholder="" />
+            <RB.ControlLabel>Total+VAT</RB.ControlLabel>{' '}
+            <RB.FormControl type="text" placeholder="" />
           </RB.FormGroup>
         </div>
       </div>
@@ -84,7 +129,7 @@ export default class App extends React.Component {
       {/* ~~~~~~~ submit ~~~~~~ */}
 
       <div className={styles.Submit}>
-        <RB.Button bsStyle="primary">submit</RB.Button>
+        <RB.Button bsStyle="primary" >submit</RB.Button>
       </div>
     </div>);
     }
